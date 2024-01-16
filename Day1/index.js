3c9510ea9597bf3d91739662450b3cb70ec8c462
Gauri <gauriM304@gmail.com>
2024-01-16 13:21:33 +0530

    solution 1

--- Day1/index.js
+++ Day1/index.js
@@ -1,2 +1,5 @@
 //Write a function createHelloWorld. It should return a new function that always returns "Hello World".
 //solution 1
+var createHelloWorld = function() {
+    return () => "Hello World";
+};
